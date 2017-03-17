b1ed33bd7585d5e742f78fe358c301fd7626212d
Vivek Hari <vivek.hari@outlook.co.nz>
2017-03-17 22:30:36 +1100

    Initial commit.

---
+++ GuessANumber/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(int argc, char* argv[]) {
+
+	// print message
+	cout << "Hello World!" << endl;
+
+	// terminate after ENTER key press
+	cout << "Press ENTER to exit...";
+	cin.ignore();
+}
